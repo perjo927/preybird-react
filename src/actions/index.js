7b1ec739798713ab6d56c0b6aab3dc1b425ca2a6
perjo927 <perjo927@student.liu.se>
2017-04-02 15:11:00 +0200

    feat(extra-points): refactor existing code

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,50 +2,42 @@ export const REQUEST_DATA = 'REQUEST_DATA'
 export const RECEIVE_DATA = 'RECEIVE_DATA'
 
 export const requestData = () => ({
-  type: REQUEST_DATA
+    type: REQUEST_DATA
 })
 
-export const receiveData = (json) => ({
-  type: RECEIVE_DATA,
-
-  dataItems: json._embedded['viaplay:blocks'][0]._embedded['viaplay:products']
-    .map((d) => {
-      return {
-        "imgL": d.content.images.landscape.url.concat("&width=480&height=270"),
-        "imgS": d.content.images.landscape.url.concat("&width=240&height=135"),
-        "title": d._links.self.title,
-        "href": d._links.self.title
-      }
-    }),
-  receivedAt: Date.now()
+export const receiveData = (json, service) => ({
+    type: RECEIVE_DATA,
+    dataItems: service.extractPlayData(json),
+    receivedAt: Date.now()
 })
 
+const fetchData = (dataService) => async dispatch => {
+    let json;
+    dispatch(requestData())
 
-const fetchData = () => dispatch => {
-  dispatch(requestData())
-  // TODO : async await try catch
-  return fetch(`https://content.viaplay.se/pc-se/serier/samtliga`)
-    .then(response => response.json())
-    .then((json) => dispatch(receiveData(json)))
-  // let response = await fetch(`https://content.viaplay.se/pc-se/serier/samtliga`)
-  // let json = await response.json();
-  // dispatch(receiveData(json))
+    try {
+        json = await dataService.get();
+    } catch (error) {
+        console.error(error);
+    }
+    return dispatch(receiveData(json, dataService))      
 }
 
 const shouldFetchData = (state) => {
-  console.log("should fetch", !!state.playData)
-  const playData = state.playData['data']
-  if (!playData) {
-    return true
-  }
-  if (playData.isFetching) {
-    return false
-  }
-  return true;
+    const playData = state.playData['data']
+
+    if (!playData) {
+        return true
+    }
+    if (playData.isFetching) {
+        return false
+    }
+
+    return true;
 }
 
-export const fetchDataIfNeeded = () => (dispatch, getState) => {
-  if (shouldFetchData(getState())) {
-    return dispatch(fetchData())
-  }
+export const fetchDataIfNeeded = (dataService) => (dispatch, getState) => {
+    if (shouldFetchData(getState())) {
+        return dispatch(fetchData(dataService))
+    }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import ContentCard from '../components/ContentCard';
 import { connect } from 'react-redux'
 import { fetchDataIfNeeded } from '../actions'
 import GridView from '../components/GridView';
+import { ImgSizes } from '../assets/media'
 import Logo from '../components/Logo';
 import logo from '../assets/logo.svg';
 import React, { Component, PropTypes } from 'react';
@@ -18,34 +19,18 @@ class App extends Component {
   }
 
   componentDidMount() {
+    const service = new PlayDataService(`https://content.viaplay.se/pc-se/serier/samtliga`);
     const { dispatch } = this.props
-    dispatch(fetchDataIfNeeded())
+    dispatch(fetchDataIfNeeded(service))
   }
-
-  // constructor(props) {
-    // this.playDataService = new PlayDataService(`https://content.viaplay.se/pc-se/serier/samtliga`);
-    // this.getPlayData(this.playDataService);
-  // }
-
-  // async getPlayData(service) {
-  //   let data;
-
-  //   try {
-  //     data = await service.get();
-  //   } catch (err) {
-  //     // console.error(err);
-  //   } finally {
-  //     const mappedData = service.extractPlayData(data)
-  //     this.setState({ "playData": mappedData });
-  //   }
-  // }
 
   getContentCards(dataItems) {
     return dataItems.map((playItem) =>
       <ContentCard key={playItem.imgS}
         srcS={playItem.imgS}
         srcL={playItem.imgL}
-        title={playItem.title}>
+        title={playItem.title}
+        sizes={ImgSizes}>
       </ContentCard>
     );
   }
@@ -57,7 +42,7 @@ class App extends Component {
     return (
       <div>
         {isEmpty
-          ? (isFetching ? <Spinner />: <h2>Error</h2>)
+          ? (isFetching ? <Spinner /> : <h2>Error</h2>)
           : <div className="App">
 
             <header>
